a17eb42d5d9351c54b8c07d78f0898a0554b76e2
Daniel Da Silva <daniel.e.dasilva@nasa.gov>
2025-09-03 19:43:57 -0400

    Working with Johns Spectrogram Code

--- dispersion_sim/utils.py
+++ dispersion_sim/utils.py
@@ -13,15 +13,15 @@ import config
 @dataclass
 class DispersionSimSetup:
     particle_long_axis: np.ndarray
+    particle_invlat_axis: np.ndarray
     particle_lat_axis: np.ndarray
     particle_energy_axis: np.ndarray
     particle_state: disco.ParticleState
 
 def setup_dispersion_sim():
     particle_long_axis = np.array([0])
-    particle_invlat_range = np.deg2rad([config.inv_lat_min, config.inv_lat_max])
-    particle_lat_range = np.arccos(np.sqrt((1 / config.particle_height) * np.cos(particle_invlat_range)**2))
-    particle_lat_axis = np.arange(*particle_lat_range, np.deg2rad(config.lat_step))
+    particle_invlat_axis = np.deg2rad(np.arange(config.inv_lat_min, config.inv_lat_max, config.inv_lat_step))
+    particle_lat_axis = np.arccos(np.sqrt((1 / config.particle_height) * np.cos(particle_invlat_axis)**2))
 
     particle_energy_axis = 10**np.linspace(np.log10(config.energy_min), np.log10(config.energy_max), config.energy_count) * u.eV
 
@@ -39,4 +39,10 @@ def setup_dispersion_sim():
     particle_state = disco.ParticleState(pos_x.flatten(), pos_y.flatten(), pos_z.flatten(),
                                      ppar.flatten(), magnetic_moment.flatten(), constants.m_p, charge)
 
-    return DispersionSimSetup(particle_long_axis, particle_lat_axis, particle_energy_axis, particle_state)
+    return DispersionSimSetup(
+        particle_long_axis,
+        particle_invlat_axis,
+        particle_lat_axis,
+        particle_energy_axis,
+        particle_state
+    )
